74380b6be138ab11e3c81e823e5af97edde9c87c
akhram.ark@gmail.com <97621761+Fabreeeze@users.noreply.github.com>
2023-06-03 18:37:13 +0530

    removed some zipping errors

--- resize.js
+++ resize.js
@@ -1,24 +1,21 @@
 // Empty
-const JSZip = require('jszip')
-// const input = document.querySelector('input');
-// const preview = document.querySelector('.preview');
-// const output=document.querySelector('#output');
-// const downloadDiv=document.querySelector('#download');
 
-
-
-         
+const zipEmUp=require('./zipping.js')
+    
 function resizeAndDownload() {
   // Step 1: Get the image file from the input element
   const curFiles=input.files;
   const filenames=[];
   const resizedFileURLS=[];
-  for(const file of curFiles){
 
+
+  var promises = [];
+  for(const file of curFiles){
+    filenames.push(file.name);
     // Step 2:convert the image file into a data URL
     const image = document.createElement('img');
     image.src = URL.createObjectURL(file);
-    console.log("image url="+image.src);
+    // console.log("image url="+image.src);
 
     //Step 3: we define a function for when the image loads
     //we define the subsequent resizing  and saving code inside a 
@@ -30,96 +27,50 @@ function resizeAndDownload() {
     //writing them with a function and calling that function on every 
     //iteration of that element allows the code inside it to consider
     //"image" as unique fo every run of code
-    image.onload=function(){
+
 
     // Step 4: Create a new canvas element and get its context
-        var canvas = document.createElement('canvas');
-        var ctx = canvas.getContext('2d');
-        // Step 5: Set the canvas width and height and draw the resized image
-        
-        var width = image.width;
-        var height = image.height;
+    var canvas = document.createElement('canvas');
+    var ctx = canvas.getContext('2d');
 
-        console.log("image width="+width);
-        console.log("image height="+height);
-        
-        canvas.width = width*0.99;
-        canvas.height = height*0.99;
-        
-        ctx.drawImage(image, 0, 0, width, height);
-        // Step 6: Get the data URL of the resized image from the canvas
-        const resizedDataURL = canvas.toDataURL('image/png');
-        // Step 7: Save the data URL string to localStorage
-        localStorage.setItem('resizedImage', resizedDataURL);
+    var promise = new Promise(function(resolve,reject){
 
-        // console.log("resized pic url="+resizedDataURL);
-    
-        // downloadFile(resizedDataURL, 'resizedImage.png');
+      image.onload=function(){
 
+          // Step 5: Set the canvas width and height and draw the resized image
+          
+          var width = image.width;
+          var height = image.height;
 
-        resizedFileURLS.push(resizedDataURL);
-    };
+          console.log("image width="+width);
+          console.log("image height="+height);
+          
+          canvas.width = width*0.99;
+          canvas.height = height*0.99;
+          
+          ctx.drawImage(image, 0, 0, width*0.99, height*0.99);
+          // Step 6: Get the data URL of the resized image from the canvas
+          const resizedDataURL = canvas.toDataURL('image/png');
+          // Step 7: Save the data URL string to localStorage
+          localStorage.setItem('resizedImage', resizedDataURL);
 
+          resizedFileURLS.push(resizedDataURL);
+          console.log("current url array="+resizedFileURLS.length);
+          resolve();
+      };
+    });
 
+    promises.push(promise);
+  }
+  console.log("name array ="+filenames);
+  console.log("url array ="+resizedFileURLS);
 
-
-        
-      filenames.push(file.name);
-    }
-    zipEmUp(resizedFileURLS,filenames);
-  
-}
-
-
-function downloadFile(data, filename) {
-    var a = document.createElement('a');
-    a.style.display = 'none';
-    a.href = data;
-    console.log("downloaded pic url="+data);
-    a.download = filename;
-    a.click();
-    window.URL.revokeObjectURL(data);
-    // document.body.appendChild(a);
+  Promise.all(promises).then(function() {
+    // Calling the zip fucntion now
+    zipEmUp(resizedFileURLS,filenames); 
+  });
 }
 
 
 
 
-function zipEmUp(fileURLArray,fileName){
-    
-    // Create a new zip object
-    var zip = new JSZip();
-
-    // Loop through the image urls and add them to the zip object
-    for (var i = 0; i < fileURLArray.length; i++) {
-       // Add each image to the zip file with a unique name
-       zip.file(fileName[i], fileURLArray[i], { binary: true });
-
-
-
-
-    //     // Fetch the image data as an array buffer using fetch API
-    //     fetch(fileURLArray[i])
-    //         .then(function(response) {
-    //         return response.arrayBuffer();
-    //         })
-    //         .then(function(imageData) {
-    //         // Add the image data to the zip object with the image name
-    //         zip.file(imageName, imageData);
-    //         })
-    //         .catch(function(error) {
-    //         // Handle any errors
-    //         console.error(error);
-    //         });
-    }
-
-    // Generate the zip file as a blob object
-    zip.generateAsync({type:"blob"}).then(function(zipFile) {
-        // Use FileSaver.js library to save the zip file
-        var a=document.createElement('a');
-        a.href=URL.createObjectURL(zipFile);
-        a.download="cropped images.zip";
-        s.click();
-    })
-
-}
--- zipping.js
+++ zipping.js
@@ -1,1 +1,47 @@
 // Empty
+
+
+const JSZip = require('jszip')
+function zipEmUp(fileURLArray,fileName){
+    
+    // Create a new zip object
+    var zip = new JSZip();
+    console.log(fileURLArray.length);
+    console.log(fileName.length);
+    // Loop through the image urls and add them to the zip object
+    for (var i = 0; i < fileName.length; i++) {
+       // Add each image to the zip file with a unique name
+
+       console.log("zip file name="+fileName[i]);
+       console.log("zip file url="+fileURLArray[i]);
+       zip.file(fileName[i], fileURLArray[i], { binary: true });
+
+
+
+
+    //     // Fetch the image data as an array buffer using fetch API
+    //     fetch(fileURLArray[i])
+    //         .then(function(response) {
+    //         return response.arrayBuffer();
+    //         })
+    //         .then(function(imageData) {
+    //         // Add the image data to the zip object with the image name
+    //         zip.file(imageName, imageData);
+    //         })
+    //         .catch(function(error) {
+    //         // Handle any errors
+    //         console.error(error);
+    //         });
+    }
+
+    // Generate the zip file as a blob object
+    zip.generateAsync({type:"blob"}).then(function(content) {
+        var a=document.createElement('a');
+        a.href=URL.createObjectURL(content);
+        a.download="cropped images.zip";
+        a.click();
+    })
+
+}
+
+module.exports=zipEmUp;
